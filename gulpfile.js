f299e725f3bbdcc7ad3599258db987ae94713862
Marton Csikos <marton.csikos@gmail.com>
2019-12-08 15:16:30 +0100

    add dev server

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const concat = require('gulp-concat');
 const del = require('del');
-const minify = require('gulp-minify');
-const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
 
 const paths = {
   src: {
+    root: './src',
     images: './src/images/*',
   	html: './src/*.html',
     sass: './src/main.scss',
@@ -18,6 +19,8 @@ const paths = {
   	public: './dist/public'
   }
 };
+
+// BUILD
 
 function clean() {
   return del(paths.dist.root);
@@ -41,17 +44,31 @@ function images() {
 function styles() {
 	return gulp.src(paths.src.sass)
 		.pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-		.pipe(cleanCSS())
+    .pipe(postcss([autoprefixer(), cssnano()]))
 		.pipe(gulp.dest(paths.dist.public));
 }
 
-module.exports = { clean, html, robots, images, styles };
-
 const build = gulp.series(clean, gulp.parallel(html, robots, images, styles));
 
+// DEVELOPMENT SERVER
+
+function server() {
+  browserSync.init({
+    port: 10530,
+    server: {
+      baseDir: "./dist",
+    },
+    ui: {
+      port: 10531
+    },
+    open: false
+  });
+}
+
+function watchAndReload() {
+  return gulp.watch(paths.src.root, gulp.series(build, browserSync.reload))
+};
+
+gulp.task('dev-server', gulp.series(build, gulp.parallel(server, watchAndReload)));
 gulp.task('build', build);
 gulp.task('default', build);
